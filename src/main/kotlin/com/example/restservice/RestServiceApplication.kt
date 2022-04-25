ba034dbb38857b0c2a1a802294755726e61f425f
n8ron <nik_abramov22@mail.ru>
2022-04-25 17:48:47 +0300

    initial commit

---
+++ src/main/kotlin/com/example/restservice/RestServiceApplication.kt
@@ -0,0 +1,108 @@
+package com.example.restservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
+
+@SpringBootApplication
+class StockServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<StockServiceApplication>(*args)
+}
+
+@RestController
+class RestController(val hwProjService: HwProjService) {
+
+    @PostMapping(value = ["/{student}/register/{subject}"])
+    fun registerStudentOnSubject(@PathVariable student: String, @PathVariable subject: String) =
+        hwProjService.registerStudentOnSubject(student, subject)
+
+    @GetMapping(value = ["/{student}/marks"])
+    fun getStudentMarks(@PathVariable student: String) =
+        hwProjService.getMarks(student)
+
+    @GetMapping(value = ["/{student}/marks/{subject}"])
+    fun getStudentMarksOnSubject(@PathVariable student: String, @PathVariable subject: String) =
+        hwProjService.getMarksFromSubject(student, subject)
+
+    @GetMapping(value = ["/{student}/marks/{subject}/avg"])
+    fun getStudentMarksOnSubjectAvg(@PathVariable student: String, @PathVariable subject: String) =
+        hwProjService.getMarksFromSubjectAvg(student, subject)
+
+    @PostMapping(value = ["/{teacher}/marks/{student}/{mark}"])
+    fun setMarkToStudent(@PathVariable teacher: String,
+                         @PathVariable student: String,
+                         @PathVariable mark: Int) = hwProjService.setMarkToStudent(teacher, student, mark)
+
+    @PostMapping(value = ["/{teacher}/register/{subject}"])
+    fun addSubject(@PathVariable teacher: String,
+                         @PathVariable subject: String) = hwProjService.addSubject(teacher, subject)
+
+    @GetMapping(value = ["/{teacher}/marks/"])
+    fun getMarksByTeacher(@PathVariable teacher: String) =
+        hwProjService.getMarksByTeacher(teacher)
+
+}
+
+
+@Service
+class HwProjService {
+    private val students = ConcurrentHashMap<String, Student>()
+    private val teachers = ConcurrentHashMap<String, Teacher>()
+
+    fun getMarks(studentName: String): Map<String, List<Int>>? {
+        return students[studentName]?.subjectsToMark
+    }
+
+    fun getMarksFromSubject(studentName: String, subjectName: String): List<Int>? {
+        return getMarks(studentName)?.get(subjectName)
+    }
+
+    fun getMarksFromSubjectAvg(studentName: String, subjectName: String): Double? {
+        return getMarks(studentName)?.get(subjectName)?.average()
+    }
+
+    fun registerStudentOnSubject(studentName: String, subject: String) {
+        students.computeIfAbsent(studentName) { Student(studentName) }
+            .subjectsToMark.computeIfAbsent(subject) { mutableListOf()}
+    }
+
+    fun setMarkToStudent(teacherName: String,
+                         studentName: String,
+                         mark: Int) {
+        teachers[teacherName]?.let { students[studentName]?.subjectsToMark?.get(it.subjectName)?.add(mark) }
+    }
+
+    fun addSubject(teacherName: String, subjectName: String) {
+        teachers[teacherName] = Teacher(teacherName, subjectName)
+    }
+
+    fun getMarksByTeacher(teacherName: String): Map<String, List<Int>?> {
+        val data: MutableMap<String, MutableList<Int>?> = mutableMapOf()
+        val subjectName = teachers[teacherName]?.subjectName
+        for (student in students.values) {
+            if (student.subjectsToMark.contains(subjectName)) {
+                data[student.name] = student.subjectsToMark[subjectName]
+            }
+        }
+        return data
+    }
+
+}
+
+
+data class Student(
+    val name: String,
+    val subjectsToMark: ConcurrentHashMap<String, MutableList<Int>> = ConcurrentHashMap<String, MutableList<Int>>()
+)
+
+data class Teacher(
+    val name: String,
+    val subjectName: String
+)
